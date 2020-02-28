ab53436d9193f26913e072032abbfe4a5bd366c9
Nikki Lin <nikkiylin@gmail.com>
2020-02-28 10:01:25 -0800

    used the styled-components to make a frame for every character

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,12 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function CharacterCard(props) {
+
+  const CharacterCardStyle= styled.div `
+    background: #efefef;
+    border: 1px solid #bfbfbf;
+    padding: 20px;
+    margin: 10px;
+  `
   return (
-    <div>
+    <CharacterCardStyle>
       <span>todo: character</span>
       <p>{props.id}</p>
       <p>{props.name}</p>
       <img src={props.image} />
-    </div>
+    </CharacterCardStyle>
   )
 }
