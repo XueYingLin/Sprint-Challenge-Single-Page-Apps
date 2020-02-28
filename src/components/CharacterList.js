78ef63d6083eea73fab099ed3ba397cb33aeb7d1
Nikki Lin <nikkiylin@gmail.com>
2020-02-28 08:57:56 -0800

    in the CharacterList file, added the useState, gt data by using axios inside the useEffect function

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,26 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function CharacterList() {
+export default function CharacterList(props) {
   // TODO: Add useState to track data from useEffect
+  const [list, setList] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    const url = "https://rickandmortyapi.com/api/character/";
+    const getList = () => {
+      axios
+      .get(`https://cors-anywhere.herokuapp.com/ ${url}`)
+      .then(response => {
+        console.log(response.data);
+        setList(response.data.results);
+      })
+      .catch(error => {
+        console.log("Server Error", error);
+      })
+    }
+    getList();
   }, []);
 
   return (
