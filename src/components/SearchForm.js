9424bdcc22ea299947b3dcd4abbee59b764f37cb
Nikki Lin <nikkiylin@gmail.com>
2020-02-28 10:26:28 -0800

    Search form show on hte webpage, but functionaly, it doesn't work

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
+export default function SearchForm(props) {
+  const onSearchChanged = (event) => {
+    props.setSearchText(event.target.value);
+  }
  
   return (
     <section className="search-form">
      // Add a search form here
+     <form style={{marginLeft: "10px"}}>
+       <input id="search" name="textfield" type="text" placeholder="Search"
+        onChange={onSearchChanged}
+        value={props.searchText}></input>
+     </form>
     </section>
   );
 }
